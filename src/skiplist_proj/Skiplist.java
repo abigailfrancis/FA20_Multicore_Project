c2800c982981850c500995df02547610b369c9d5
ccowden <catie.cowden@gmail.com>
2020-11-15 17:52:42 -0800

    change find() signature, cleanup files, remove bin

---
+++ src/skiplist_proj/Skiplist.java
@@ -0,0 +1,31 @@
+package skiplist_proj;
+
+import java.util.List;
+
+/**
+ * Interface for the Skiplist implementations
+ */
+public interface Skiplist {
+    /**
+     * Adds a new element to the Skiplist
+     * @param value the value to be added to the Skiplist
+     * @return a boolean indicating whether the operation was successful
+     */
+    boolean add(Integer value);
+
+    /**
+     * Removes a value from the Skiplist
+     * @param value the value to be removed from the skiplist
+     * @return a boolean indicating whether the operation was successful
+     */
+    boolean rm(Integer value);
+
+    /**
+     * Searches for a specific value in the Skiplist
+     * @param value the value to search for in the Skiplist
+     * @param preds the list of predecessors for the node we are searching for
+     * @param succs the list of successors for the node we are searching for
+     * @return The level where the value was found, otherwise -1
+     */
+    Integer find(Integer value, List<Node> preds, List<Node> succs);
+}
--- src/skiplist_proj/Tests.java
+++ src/skiplist_proj/Tests.java
@@ -1,21 +1,28 @@
 package skiplist_proj;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.*;
-import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class Tests {
 
-    public Tests(myDB q)
+    private Skiplist skiplist;
+
+    public Tests(Skiplist skiplist)
     {
-        
+        this.skiplist = skiplist;
+    }
+
+    @Parameterized.Parameters()
+    public static Collection<Skiplist> data() {
+        ArrayList<Skiplist> testParams = new ArrayList<>();
+
+        // Run with both types of Skiplist
+        testParams.add(new Lock());
+        testParams.add(new LockFree());
+
+        return testParams;
     }
 }
