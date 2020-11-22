92ccf9e6e8fc210228c346aa82fdfa1f90931552
ccowden <catie.cowden@gmail.com>
2020-11-22 15:56:08 -0800

    Initial implementation of lockbased Remove()

--- src/skiplist_proj/LockBasedSkiplist.java
+++ src/skiplist_proj/LockBasedSkiplist.java
@@ -2,11 +2,13 @@ package skiplist_proj;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class LockBasedSkiplist implements Skiplist
 {
     private AtomicReference<Node> head = new AtomicReference<>();
+    private Random rnd = new Random();
 
     public LockBasedSkiplist(Node head)
     {
@@ -20,24 +22,35 @@ public class LockBasedSkiplist implements Skiplist
     @Override
     public boolean add(Integer value)
     {
-        int topLevel = MAX_HEIGHT;
+        // Chosoe a random level (0 <= x < MAX_HEIGHT) for the new node's top level
+        int topLevel = chooseRandomTopLevel();
 
+        // Initialize empty preds and succs lists
         List<AtomicReference<Node>> preds = new ArrayList<>();
         List<AtomicReference<Node>> succs = new ArrayList<>();
 
         for (int i = 0; i < MAX_HEIGHT; i++)
         {
-            // Initialize empty values for preds and succs
             preds.add(i, null);
             succs.add(i, null);
         }
 
-        while (true) {
-            Integer lFound = find(value, preds, succs);
-            if (lFound != -1) {
-                AtomicReference<Node> nodeFound = succs.get(lFound);
-                if (!nodeFound.get().marked) {
-                    while (!nodeFound.get().fullyLinked) {
+        while (true)
+        {
+            // Determine if the value is already in the Skiplist
+            Integer levelFound = find(value, preds, succs);
+
+            // If the value is already in the Skiplist
+            if (levelFound != -1)
+            {
+                // Get a reference to the successor node
+                AtomicReference<Node> nodeFound = succs.get(levelFound);
+
+                // If the successor node is not marked
+                if (!nodeFound.get().isMarked()) {
+
+                    // Wait for it to be fully-linked
+                    while (!nodeFound.get().isFullyLinked()) {
                         // no-op
                     }
 
@@ -48,55 +61,162 @@ public class LockBasedSkiplist implements Skiplist
             }
 
             int highestLocked = -1;
-            try {
+
+            try
+            {
                 AtomicReference<Node> pred;
                 AtomicReference<Node> succ;
 
                 boolean valid = true;
 
+                // Starting at the bottom of the Skiplist
+                // Lock all of the predecessor nodes
                 for (int level = 0; valid && (level < topLevel); level++)
                 {
                     pred = preds.get(level);
                     succ = succs.get(level);
                     pred.get().lock();
                     highestLocked = level;
-                    valid = !pred.get().marked && !succ.get().marked && pred.get().next[level] == succ;
+                    valid = !pred.get().isMarked() && !succ.get().isMarked() && pred.get().next[level] == succ;
                 }
 
                 if (!valid) continue;
 
+                // Create the new Node
                 Node node = new Node(value, topLevel);
-
                 AtomicReference<Node> newNode = new AtomicReference<>(node);
 
+                // Starting at the bottom of the Skiplist
+                // Attach the new Node's successors
                 for (int level = 0; level < topLevel; level++)
                 {
-                    // Null exception here
                     newNode.get().next[level].set(succs.get(level).get());
                 }
 
+                // Starting at the bottom of the Skiplist
+                // Attach the new Node's predecessors
                 for (int level = 0; level < topLevel; level++)
                 {
                     preds.get(level).get().next[level].set(newNode.get());
                 }
 
-                newNode.get().fullyLinked = true;
+                newNode.get().setFullyLinked(true);
+
                 return true;
             }
             finally
             {
-                for (int level = 0; level < highestLocked; level++)
-                {
-                    preds.get(level).get().unlock();
-                }
+                // Unlock at all levels
+                unlockAtAllLevels(preds, highestLocked);
             }
         }
     }
 
+    /**
+     * @param value the value to be removed from the Skiplist
+     * @return True, if the Integer was removed successfully
+     */
     @Override
-    public boolean rm(Integer value) {
-        // Todo: Implement method
-        return false;
+    public boolean rm(Integer value)
+    {
+        AtomicReference<Node> victim = null;
+        boolean isMarked = false;
+        Integer topLevel = -1;
+
+        // Initialize empty preds and succs lists
+        List<AtomicReference<Node>> preds = new ArrayList<>();
+        List<AtomicReference<Node>> succs = new ArrayList<>();
+
+        for (int i = 0; i < MAX_HEIGHT; i++)
+        {
+            preds.add(i, null);
+            succs.add(i, null);
+        }
+
+        while (true)
+        {
+            // Determine if the value is already in the Skiplist
+            Integer levelFound = find(value, preds, succs);
+
+            // If the value is already in the Skiplist
+            if (levelFound != -1)
+            {
+                // Get a reference to it
+                victim = succs.get(levelFound);
+            }
+
+            // If the node is marked OR
+            // If the node is present in the Skiplist, fully linked, not marked, and is on the right level
+            if (isMarked ||
+                    (levelFound != -1
+                            && victim != null
+                            && (victim.get().isFullyLinked()
+                            && victim.get().getTopLevel().equals(levelFound)
+                            && !victim.get().isMarked())))
+            {
+                if (!isMarked)
+                {
+                    // Determine the highest level of the Skiplist that this value
+                    // is present on.
+                    topLevel = victim.get().getTopLevel();
+
+                    victim.get().lock();
+
+                    if (victim.get().isMarked())
+                    {
+                        victim.get().unlock();
+                        return false;
+                    }
+
+                    victim.get().setMarked(true);
+                    isMarked = true;
+                }
+
+                Integer highestLocked = -1;
+
+                try
+                {
+                    AtomicReference<Node> pred;
+
+                    boolean valid = true;
+
+                    // Starting at the bottom of the Skiplist
+                    // Lock the predecessor nodes
+                    for (int level = 0; valid && (level <= topLevel); level++)
+                    {
+                        pred = preds.get(level);
+                        pred.get().lock();
+                        highestLocked = level;
+                        valid = !pred.get().isMarked() && pred.get().next[level] == victim;
+                    }
+
+                    if (!valid) continue;
+
+                    // Starting at the top of the Skiplist
+                    // Reassign the predecessor nodes
+                    for (int level = topLevel; level >= 0; level--)
+                    {
+                        AtomicReference<Node> newSuccessor = victim.get().next[level];
+                        Node predecessor = preds.get(level).get();
+                        predecessor.next[level].set(newSuccessor.get());
+                    }
+
+                    // The victim has been removed, unlock it
+                    // Todo, re-add this?
+                    // victim.get().unlock();
+
+                    return true;
+                }
+                finally
+                {
+                    // Unlock at all levels
+                    unlockAtAllLevels(preds, highestLocked);
+                }
+            }
+            else {
+                return false;
+            }
+        }
     }
 
     /**
@@ -131,7 +251,7 @@ public class LockBasedSkiplist implements Skiplist
             }
 
             // If the key is found, update the return variable
-            if (lFound == -1 && value == curr.get().getItem())
+            if (lFound == -1 && curr.get().getItem().equals(value))
             {
                 lFound = level;
             }
@@ -143,4 +263,23 @@ public class LockBasedSkiplist implements Skiplist
 
         return lFound;
 	}
+
+    /**
+     * @return The highest level (0 <= x < MAX_HEIGHT) of the Skiplist that the new node will be added to
+     */
+    private int chooseRandomTopLevel() {
+        return rnd.nextInt(MAX_HEIGHT);
+    }
+
+    /**
+     * Unlock the nodes in the collection between level 0 & highestLocked
+     * @param nodeCollection The collection of nodes to be unlocked
+     * @param highestLocked The highest level where the node is present
+     */
+    private void unlockAtAllLevels(List<AtomicReference<Node>> nodeCollection, int highestLocked) {
+        for (int level = 0; level <= highestLocked; level++)
+        {
+            nodeCollection.get(level).get().unlock();
+        }
+    }
 }
